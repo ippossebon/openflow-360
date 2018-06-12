c0c311c96555d9858325ce4bd885469aec57ea2f
Isadora <isadorapossebon@gmail.com>
2018-06-11 23:14:22 -0300

    Change support to OF 1.0

--- controller/topologies/test1.py
+++ controller/topologies/test1.py
@@ -8,9 +8,9 @@ from mininet.node import OVSSwitch, Controller, RemoteController
 
 class SingleSwitchTopo(Topo):
     def build(self):
-        s1 = self.addSwitch('s1', protocols='OpenFlow13')
-        s2 = self.addSwitch('s2', protocols='OpenFlow13')
-        s3 = self.addSwitch('s3', protocols='OpenFlow13')
+        s1 = self.addSwitch('s1')
+        s2 = self.addSwitch('s2')
+        s3 = self.addSwitch('s3')
 
         h1 = self.addHost('h1', mac="00:00:00:00:00:01", ip="10.0.0.1/12")
         h2 = self.addHost('h2', mac="00:00:00:00:00:02", ip="10.0.0.2/12")
