d98d06d489b6274a882f43954f8753211275f065
Isadora <isadorapossebon@gmail.com>
2018-06-11 23:09:44 -0300

    Add redundant links

--- controller/topologies/test1.py
+++ controller/topologies/test1.py
@@ -12,11 +12,11 @@ class SingleSwitchTopo(Topo):
         s2 = self.addSwitch('s2', protocols='OpenFlow13')
         s3 = self.addSwitch('s3', protocols='OpenFlow13')
 
-        h1 = self.addHost('h1', mac="00:00:00:00:11:11", ip="10.0.0.1/24")
-        h2 = self.addHost('h2', mac="00:00:00:00:11:12", ip="10.0.0.2/24")
-        h3 = self.addHost('h3', mac="00:00:00:00:11:13", ip="10.0.0.3/24")
-        h4 = self.addHost('h4', mac="00:00:00:00:11:14", ip="10.0.0.4/24")
-        h5 = self.addHost('h5', mac="00:00:00:00:11:15", ip="10.0.0.5/24")
+        h1 = self.addHost('h1', mac="00:00:00:00:00:01", ip="10.0.0.1/12")
+        h2 = self.addHost('h2', mac="00:00:00:00:00:02", ip="10.0.0.2/12")
+        h3 = self.addHost('h3', mac="00:00:00:00:00:03", ip="10.0.0.3/12")
+        h4 = self.addHost('h4', mac="00:00:00:00:00:04", ip="10.0.0.4/12")
+        h5 = self.addHost('h5', mac="00:00:00:00:00:05", ip="10.0.0.5/12")
 
         self.addLink(h3, s1)
         self.addLink(h4, s1)
@@ -27,8 +27,11 @@ class SingleSwitchTopo(Topo):
         self.addLink(h2, s3)
 
         self.addLink(s1, s2)
+        self.addLink(s1, s3)
+        self.addLink(s2, s1)
         self.addLink(s2, s3)
         self.addLink(s3, s1)
+        self.addLink(s3, s2)
 
 
 if __name__ == '__main__':
