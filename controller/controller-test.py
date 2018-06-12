7beef9e385df767d6114a6d986bfb0e487b1a7a8
Isadora <isadorapossebon@gmail.com>
2018-06-11 23:03:48 -0300

    Add OF 1.3 support

--- controller/controller-test.py
+++ controller/controller-test.py
@@ -3,7 +3,7 @@ from ryu.controller import ofp_event
 from ryu.controller.handler import CONFIG_DISPATCHER
 from ryu.controller.handler import MAIN_DISPATCHER
 from ryu.controller.handler import set_ev_cls
-from ryu.ofproto import ofproto_v1_0
+from ryu.ofproto import ofproto_v1_3
 from ryu.lib import dpid as dpid_lib
 from ryu.lib.packet import packet
 from ryu.lib.packet import ethernet
@@ -22,7 +22,7 @@ import logging
 
 
 class SimpleSwitch13(app_manager.RyuApp):
-    OFP_VERSIONS = [ofproto_v1_0.OFP_VERSION]
+    OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
 
     def __init__(self, *args, **kwargs):
         super(SimpleSwitch13, self).__init__(*args, **kwargs)
