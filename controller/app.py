aa0a16b6c415c23818705b39a217840ba66b94df
Isadora <isadorapossebon@gmail.com>
2018-06-11 21:11:47 -0300

    Add UTF8 support

--- controller/app.py
+++ controller/app.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 # Copyright (C) 2011 Nippon Telegraph and Telephone Corporation.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
