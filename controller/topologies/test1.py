6c2de2ebe3e0f47d09bcd4ace0f547790d5d4024
Isadora Possebon <isadora.possebon@corp.globo.com>
2018-06-13 09:40:42 -0300

    Add fixed topology

--- controller/topologies/test1.py
+++ controller/topologies/test1.py
@@ -8,9 +8,9 @@ from mininet.node import OVSSwitch, Controller, RemoteController
 
 class SingleSwitchTopo(Topo):
     def build(self):
-        s1 = self.addSwitch('s1')
-        s2 = self.addSwitch('s2')
-        s3 = self.addSwitch('s3')
+        s1 = self.addSwitch('s1', mac="11:00:00:00:00:01")
+        s2 = self.addSwitch('s2', mac="11:00:00:00:00:02")
+        s3 = self.addSwitch('s3', mac="11:00:00:00:00:03")
 
         h1 = self.addHost('h1', mac="00:00:00:00:00:01", ip="10.0.0.1/12")
         h2 = self.addHost('h2', mac="00:00:00:00:00:02", ip="10.0.0.2/12")
