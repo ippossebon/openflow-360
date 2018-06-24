51ca180267c96dddf3f53a09b7b756b23ebf0dc4
Isadora <isadorapossebon@gmail.com>
2018-06-24 20:38:26 -0300

    Add python server

--- server.py
+++ server.py
@@ -1,33 +1,7 @@
 #!/usr/bin/env python
 
-from http.server import BaseHTTPRequestHandler, HTTPServer
+from http.server import SimpleHTTPRequestHandler, HTTPServer
 
-# HTTPRequestHandler class
-class testHTTPServer_RequestHandler(BaseHTTPRequestHandler):
-
-  # GET
-  def do_GET(self):
-        # Send response status code
-        self.send_response(200)
-
-        # Send headers
-        self.send_header('Content-type','text/html')
-        self.end_headers()
-
-        # Send message back to client
-            with open('/var/www/html/video12x4/12x4/', 'rb') as file:
-                self.wfile.write(file.read()) # Read the file and send the contents
-        return
-
-def run():
-    print('Starting server...')
-
-    # Server settings
-    # Choose port 8080, for port 80, which is normally used for a http server, you need root access
-    server_address = ('127.0.0.1', 8081)
-    httpd = HTTPServer(server_address, testHTTPServer_RequestHandler)
-    print('Running server...')
-    httpd.serve_forever()
-
-
-run()
+server_address = ('', 8000)                                                                                                                                                                    
+httpd = HTTPServer(server_address, SimpleHTTPRequestHandler)
+httpd.serve_forever()
