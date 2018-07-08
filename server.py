b1f962ad7e5e00c335b473daa634d81368201bef
Isadora <isadorapossebon@gmail.com>
2018-07-08 08:30:05 -0300

    Add multiple controllers for heuristics

--- server.py
+++ server.py
@@ -2,6 +2,7 @@
 
 from http.server import SimpleHTTPRequestHandler, HTTPServer
 
-server_address = ('', 8000)                                                                                                                                                                    
+server_address = ('', 8000)
 httpd = HTTPServer(server_address, SimpleHTTPRequestHandler)
+
 httpd.serve_forever()
